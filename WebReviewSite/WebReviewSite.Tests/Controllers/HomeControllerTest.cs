8f07fe1d7011ff9a71ab0a034cfb0ae5216e61c3
Cameron Wagstaff <cam.wagstaff@gmail.com>
2018-05-03 13:09:06 -0400

    MVC proof of concept w/minimal restaurant displaying

--- WebReviewSite/WebReviewSite.Tests/Controllers/HomeControllerTest.cs
+++ WebReviewSite/WebReviewSite.Tests/Controllers/HomeControllerTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReviewSiteLogic;
 using WebReviewSite;
 using WebReviewSite.Controllers;
 
--- WebReviewSite/WebReviewSite/Controllers/HomeController.cs
+++ WebReviewSite/WebReviewSite/Controllers/HomeController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ReviewSiteLogic;
 using System.Web.Mvc;
 
 namespace WebReviewSite.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
+    public class HomeController : Controller {
+        private readonly Session _session;
+
+        public HomeController() {
+            _session = new Session("TestReviewSiteContext");
+        }
+
+        public ActionResult Index() {
+            var topRestaurants = _session.ViewTopRestaurants();
+            return View(topRestaurants);
+        }
+
+        public ActionResult ListRestaurants() {
+            var restaurantList = _session.ViewRestaurantsSortedRatingDesc();
+            return View(restaurantList);
+        }
+
+        public ActionResult ViewRestaurant(int id) {
+            var restaurant = _session.ViewRestaurant(id);
+            return View(restaurant);
         }
 
         public ActionResult About()
