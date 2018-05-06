e56d243d7dfded237951c957e12f02cc10c345c6
Cameron Wagstaff <cam.wagstaff@gmail.com>
2018-05-06 18:19:35 -0400

    Added content to about and contact pages.

--- WebReviewSite/WebReviewSite/Controllers/HomeController.cs
+++ WebReviewSite/WebReviewSite/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using ReviewSiteLogic;
+﻿using ReviewSiteLogic;
 using System.Web.Mvc;
 using ReviewSiteLogic.Render;
 
